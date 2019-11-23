e8d3bc10874ef4dcc6122693b0c310af7a703544
hazrak <hecham.azrak.521@gmail.com>
2019-11-23 20:48:41 +0100

    add slot for each button, showing result

--- calculatrice/mainwindow.cpp
+++ calculatrice/mainwindow.cpp
@@ -6,9 +6,127 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_result_temp = new QList<QString>;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showResult()
+{
+    m_result.clear();
+    for(QString w : *m_result_temp)
+    {
+        m_result.append(w);
+    }
+    ui->textBrowser->setText(m_result);
+}
+
+void MainWindow::on_pushButton_0_clicked()
+{
+    m_result_temp->push_back("0");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    m_result_temp->push_back("1");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    m_result_temp->push_back("2");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    m_result_temp->push_back("3");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    m_result_temp->push_back("4");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    m_result_temp->push_back("5");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    m_result_temp->push_back("6");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    m_result_temp->push_back("7");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    m_result_temp->push_back("8");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    m_result_temp->push_back("9");
+    ui->textBrowser->clear();
+    showResult();
+}
+
+void MainWindow::on_pushButton_plus_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_moins_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_fois_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_divise_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_clean_clicked()
+{
+    ui->textBrowser->clear();
+    m_result_temp = new QList<QString>;
+}
+
+void MainWindow::on_pushButton_calculate_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_comma_clicked()
+{
+    m_result_temp->push_back(".");
 }
--- calculatrice/mainwindow.h
+++ calculatrice/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTextStream>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +15,32 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void showResult();
+
+private slots:
+    void on_pushButton_0_clicked();
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+    void on_pushButton_plus_clicked();
+    void on_pushButton_moins_clicked();
+    void on_pushButton_fois_clicked();
+    void on_pushButton_divise_clicked();
+    void on_pushButton_clean_clicked();
+    void on_pushButton_calculate_clicked();
+    void on_pushButton_comma_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    QString m_result;
+    QList<QString> *m_result_temp;
 };
 
 #endif // MAINWINDOW_H
